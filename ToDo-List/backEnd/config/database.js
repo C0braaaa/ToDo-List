b9e0eb02988c8218d107b524118a630abb0f782e
C0braaaa <thanhhieu151204@gmail.com>
2025-06-02 21:07:59 +0700

    ToDo-List

--- ToDo-List/backEnd/config/database.js
+++ ToDo-List/backEnd/config/database.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
 const mysql = require('mysql2');
+require('dotenv').config(); // Nạp biến môi trường từ .env
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST,      
+    user: process.env.DB_USER,           
+    password: process.env.DB_PASSWORD,  
+    database: process.env.DB_NAME     
 };
 
 const connection = mysql.createConnection(dbConfig);
